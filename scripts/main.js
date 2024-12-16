0458ec3d2858a156e728aa1f78abbc5f8cd6c166
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2024-12-16 13:09:49 +0000

    solution v02.2

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,15 @@ const transitionTime = 400;
 const menuLinks = document.querySelectorAll('.menu__link');
 const pageTitle = document.querySelector('.hero__title');
 const parallaxImgs = document.querySelectorAll('.parallax-img');
+const zoomImgs = document.querySelectorAll('.zoom-img');
+const imgs3d = document.querySelectorAll('.img-3d');
 
 window.addEventListener('load', () => {
   addGlassmorphToHeader();
 });
 window.addEventListener('scroll', () => {
   addGlassmorphToHeader();
+  toggleAnimItems();
 });
 
 for (const link of menuLinks) {
@@ -38,6 +41,20 @@ for (const item of parallaxImgs) {
   setCSSProperty(itemContainer, '--image-url', newUrl);
 }
 
+// for (const item of imgs3d) {
+//   item.addEventListener("mousemove", e => {
+//     const rect = e.target.getBoundingClientRect();
+//     const ratioX = (e.clientX - rect.left) / rect.width;
+//     const ratioY = (e.clientY - rect.top) / rect.height;
+
+//     const degX = 20 - 40 * ratioY;
+//     const degY = 40 * ratioX - 20;
+
+//     item.style.setProperty("--x", degX + "deg");
+//     item.style.setProperty("--y", degY + "deg");
+//   });
+// }
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   form.reset();
@@ -49,6 +66,17 @@ burger.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
 
+for (const img of zoomImgs) {
+  const imgContainer = img.parentElement;
+  const imgSrc = img.getAttribute('src');
+  const newUrl = `url("../../.${imgSrc}")`;
+
+  imgContainer.addEventListener("mousemove", e => {
+    imgContainer.style.backgroundPosition = -e.offsetX + 'px ' + -e.offsetY + 'px';
+  });
+  
+  setCSSProperty(imgContainer, '--image-url', newUrl);
+}
 
 function lockPage() {
   const rightPaddingValue = window.innerWidth - document.documentElement.clientWidth + 'px';
@@ -84,3 +112,23 @@ function addGlassmorphToHeader() {
     setCSSProperty(header, '--before-opacity', '1');
   }
 }
+function toggleAnimItems(selector = '.scroll-anim') {
+  const animItems = document.querySelectorAll(selector);
+  
+    animItems.forEach(item => {
+      if (isInView(item, 0.35)) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    })
+  }
+  
+  function isInView(elem, persent= 0.35) {
+    const rect = elem.getBoundingClientRect();
+    const elemHeight = elem.offsetHeight;
+    const visiblePart = elemHeight * persent;
+    
+    return rect.bottom > 0 && rect.top < (
+      window.innerHeight - visiblePart || document.documentElement.clientHeight - visiblePart);
+  }
